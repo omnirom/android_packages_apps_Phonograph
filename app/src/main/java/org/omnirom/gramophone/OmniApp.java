536e1b824948616eeb540609d9dc436c18eee92c
oshmoun <oshmoun100@googlemail.com>
2017-04-08 04:23:47 +0200

    OmniApp: Add shortcuts manager
    Change-Id: I26d1fa9d8bc852d7574642ca187dd3d6fe10c550

--- app/src/main/java/org/omnirom/gramophone/OmniApp.java
+++ app/src/main/java/org/omnirom/gramophone/OmniApp.java
@@ -1,6 +1,9 @@
 package org.omnirom.gramophone;
 
 import android.app.Application;
+import android.os.Build;
+
+import com.kabouzeid.gramophone.appshortcuts.DynamicShortcutManager;
 
 public class OmniApp extends Application {
     public static final String TAG = OmniApp.class.getSimpleName();
@@ -8,5 +11,10 @@ public class OmniApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //Set up dynamic shortcuts
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            new DynamicShortcutManager(this).initDynamicShortcuts();
+        }
     }
 }
